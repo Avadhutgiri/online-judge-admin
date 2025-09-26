e2c3e53509ee6d859788c29f8dbca9f067d5d9e0
Avadhutgiri <avadg252@gmail.com>
2025-09-26 10:38:37 +0530

    testing

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// const API_BASE_URL = 'https://onlinejudge.duckdns.org/api';
+const API_BASE_URL = 'https://onlinejudge.duckdns.org/api';
 
-const API_BASE_URL = ' https://toylike-nicolette-unsensualistic.ngrok-free.dev/api';
+// const API_BASE_URL = ' https://toylike-nicolette-unsensualistic.ngrok-free.dev/api';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
